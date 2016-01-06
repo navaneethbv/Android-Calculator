f93a5b0d736c0adeb1bda0239e3d876ef26988cc
Navaneeth <navaneethbv@gmail.com>
2016-01-06 17:37:37 -0600

    Adding New Files

---
+++ app/src/main/java/com/example/test/android_calculator/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.test.android_calculator;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    String total = "";
+    double v1,v2;
+    String sign = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onClick(View v) {
+        Button button = (Button) v;
+        String str = button.getText().toString();
+        total += str;
+        EditText edit = (EditText)findViewById(R.id.editText);
+        edit.setText(total);
+    }
+
+    public void onAdd(View v){
+        v1 = Double.parseDouble(total);
+        total = "";
+        Button button = (Button) v;
+        String str = button.getText().toString();
+        sign = str;
+        EditText edit = (EditText)findViewById(R.id.editText);
+        edit.setText("");
+    }
+
+    public void onCalculate(View v){
+        EditText edit = (EditText)findViewById(R.id.editText);
+        String str2 = edit.getText().toString();
+        v2 = Double.parseDouble(str2);
+        double grand_total=0;
+        if(sign.equals("+")){
+            grand_total = v1+v2;
+        }
+        else if(sign.equals("-")){
+            grand_total = v1-v2;
+        }
+        else if(sign.equals("*")){
+            grand_total = v1*v2;
+        }
+        else if(sign.equals("/")){
+            grand_total = v1/v2;
+        }
+        edit.setText(grand_total+"");
+    }
+
+    public void onClear(View v){
+        EditText edit = (EditText)findViewById(R.id.editText);
+        edit.setText("");
+        total = "";
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
